2c3916a61387e52d35ce1fbdbd14d48457123c0c
angelDova <Angelfcb01@yahoo.com>
2023-09-18 15:43:47 -0700

    update

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,6 +1,8 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const ServerIdLayout = async ({
   children,
@@ -18,10 +20,26 @@ const ServerIdLayout = async ({
   const server = await db.server.findUnique({
     where: {
       id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
     },
   });
 
-  return <div className="">{children}</div>;
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
 };
 
 export default ServerIdLayout;
