8d41eebd4ce2ee6da025798f47c78a29e083f287
angelDova <Angelfcb01@yahoo.com>
2023-09-19 20:03:42 -0700

    update

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -39,7 +39,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
           <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
-            invite people
+            Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
